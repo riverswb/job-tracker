2a015b9243e4bee3a0fa13e74a5ab34a44598219
Brett Rivers <riverswb@gmail.com>
2016-12-19 08:01:48 -0700

    Dashboard shows the jobs per location

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,11 +2,12 @@ class CompaniesController < ApplicationController
   def dashboard
     @companies = Company.all
     @sorted_companies = find_top_three_companies
+    @jobs_count = Job.group(:city).count("id")
   end
 
   def find_top_three_companies
     averages = Company.joins(:jobs).group(:company_id).average("level_of_interest")
-    top_companies = averages.values.sort.take(3)
+    top_companies = averages.values.sort.reverse.take(3)
     found = {}
     top_companies.each do |avg|
       found[Company.find(averages.key(avg))] = avg
--- spec/features/analysis/user_can_visit_the_dashboard_to_see_analytics_spec.rb
+++ spec/features/analysis/user_can_visit_the_dashboard_to_see_analytics_spec.rb
@@ -6,8 +6,18 @@ describe "User visits the dashboard and sees analytics" do
 
     expect(page).to have_content("Companies Dashboard")
     expect(page).to have_content("Top 3 Companies by Interest:")
-    expect(page.find "li:nth-child(1)").to have_content("Bad company")
-    expect(page.find "li:nth-child(2)").to have_content("Meh company")
-    expect(page.find "li:nth-child(3)").to have_content("Good company")
+    expect(page.find "ol li:nth-child(1)").to have_content("Good company")
+    expect(page.find "ol li:nth-child(2)").to have_content("Meh company")
+    expect(page.find "ol li:nth-child(3)").to have_content("Bad company")
+  end
+
+  scenario "user sees a count of jobs by location" do
+    visit "/dashboard"
+
+    expect(page).to have_content("Companies Dashboard")
+    expect(page).to have_content("Number of Jobs per Location")
+    expect(page).to have_content("Fort Collins : 2")
+    expect(page).to have_content("Denver : 2")
+    expect(page).to have_content("Colorado Springs : 1")
   end
 end
